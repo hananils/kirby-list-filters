88afabae39f828d649f8316d7ee394c148334230
Nils Hörrmann <nils.hoerrmann@hananils.de>
2026-09-25 09:27:45 +0200

    Update composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit5a14daf6d1ba42b6ba744b2f23d1fa6b
 {
+    public static $prefixLengthsPsr4 = array (
+        'K' =>
+        array (
+            'Kirby\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Kirby\\' =>
+        array (
+            0 => __DIR__ . '/..' . '/getkirby/composer-installer/src',
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -13,6 +27,8 @@ class ComposerStaticInit5a14daf6d1ba42b6ba744b2f23d1fa6b
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit5a14daf6d1ba42b6ba744b2f23d1fa6b::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit5a14daf6d1ba42b6ba744b2f23d1fa6b::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit5a14daf6d1ba42b6ba744b2f23d1fa6b::$classMap;
 
         }, null, ClassLoader::class);
